c027d279fd508de66098074167443beab7f240fe
Nicolas S. Montanaro <nsmon@rbox.co>
2018-03-16 18:11:41 -0400

    some more executors

--- cpu/executors.go
+++ cpu/executors.go
@@ -10,19 +10,18 @@ import (
 // Prevents having to set MMU pointer as a field on the CPU struct
 var GbMMU *mmu.GBMMU
 
-func (gbcpu *GBCPU) LDrr_nn(reg1, reg2 *byte) {
+func (gbcpu *GBCPU) LDrrnn(reg1, reg2 *byte) {
 	operands := gbcpu.getOperands(2)
 
-	// Little Endian, so reversed
 	gbcpu.Regs.writePair(reg1, reg2, operands)
 }
 
-func (gbcpu *GBCPU) LDr_n(reg *byte) {
+func (gbcpu *GBCPU) LDrn(reg *byte) {
 	operand := gbcpu.getOperands(1)
 	*reg = operand[0]
 }
 
-func (gbcpu *GBCPU) LDrr_r(reg1, reg2, op *byte) {
+func (gbcpu *GBCPU) LDrrr(reg1, reg2, op *byte) {
 	gbcpu.Regs.writePair(reg1, reg2, []byte{*op, *op})
 }
 
@@ -35,6 +34,11 @@ func (gbcpu *GBCPU) INCr(reg *byte) {
 	*reg++
 }
 
+func (gbcpu *GBCPU) INCrn(reg *byte) {
+	operand := gbcpu.getOperands(1)
+	*reg = operand[0]
+}
+
 func (gbcpu *GBCPU) DECr(reg *byte) {
 	*reg--
 }
@@ -44,30 +48,82 @@ func (gbcpu *GBCPU) DECrr(reg1, reg2 *byte) {
 	*reg2--
 }
 
-func (gbcpu *GBCPU) RLCA(reg *byte) {
-	*reg = *reg << 8
+func (gbcpu *GBCPU) RLCA() {
+	gbcpu.Regs.a = gbcpu.Regs.a << 8
 }
 
-func (gbcpu *GBCPU) LDnn_SP() {
+func (gbcpu *GBCPU) RLA() {
+	gbcpu.Regs.a = gbcpu.Regs.a << 9
+}
+
+func (gbcpu *GBCPU) RRCA() {
+	gbcpu.Regs.a = gbcpu.Regs.a >> 8
+}
+
+func (gbcpu *GBCPU) RRA() {
+	gbcpu.Regs.a = gbcpu.Regs.a >> 9
+}
+
+func (gbcpu *GBCPU) LDaaSP() {
 	operands := gbcpu.getOperands(2)
-	gbcpu.Regs.sp = operands
+	val := gbcpu.getValCartAddr(&operands[1], &operands[0], 2)
+	gbcpu.Regs.sp = val
 }
 
-func (gbcpu *GBCPU) ADDrr_rr(left1, left2, right1, right2 *byte) {
+func (gbcpu *GBCPU) ADDrrrr(left1, left2, right1, right2 *byte) {
 	*left1 = *right1
 	*left2 = *right2
 }
 
 // a1, s2 are 8-bit components of a 16-bit address
 // Loads value at location a1a2 into reg
-func (gbcpu *GBCPU) LDr_rr(reg, a1, a2 *byte) {
-	val := gbcpu.getValCartAddr(a1, a2)
-	*reg = val
+func (gbcpu *GBCPU) LDraa(reg, a1, a2 *byte) {
+	val := gbcpu.getValCartAddr(a1, a2, 1)
+	*reg = val[0]
+}
+
+func (gbcpu *GBCPU) LDaar(a1, a2, reg *byte) {
+	val := gbcpu.getValCartAddr(a1, a2, 1)
+	*reg = val[0]
+}
+
+// Set value at address a1a2 to value in reg
+// Increment reg
+func (gbcpu *GBCPU) LDIaaR(a1, a2, reg *byte) {
+	GbMMU.Cart.MBC[binary.LittleEndian.Uint16([]byte{*a1, *a2})] = *reg
+	*reg++
+}
+
+// Set value in reg to  value at address a1a2
+// Increment reg
+func (gbcpu *GBCPU) LDIRaa(reg, a1, a2 *byte) {
+	*reg = GbMMU.Cart.MBC[binary.LittleEndian.Uint16([]byte{*a1, *a2})]
+	*reg++
 }
 
 func (gbcpu *GBCPU) JPaa() {
 	jmpAddr := gbcpu.getOperands(2)
 	gbcpu.Regs.PC = jmpAddr
+}
+
+// Add byte at PC + 1 to PC, and set PC to that value
+func (gbcpu *GBCPU) JRn() {
+	operand := gbcpu.getOperands(1)
+	newPC := gbcpu.sliceToInt(gbcpu.Regs.PC) + uint16(operand[0])
+	binary.LittleEndian.PutUint16(gbcpu.Regs.PC, newPC)
+}
+
+func (gbcpu *GBCPU) JRZn() {
+	// TODO
+}
+
+func (gbcpu *GBCPU) JRNZn() {
+	// TODO
+}
+
+func (gbcpu *GBCPU) DAA() {
+	// TODO
+	// Reference: http://forums.nesdev.com/viewtopic.php?t=9088
 }
 
 func (gbcpu *GBCPU) sliceToInt(slice []byte) uint16 {
@@ -81,6 +137,8 @@ func (gbcpu *GBCPU) getOperands(number uint16) []byte {
 	return GbMMU.Cart.MBC[begin:end]
 }
 
-func (gbcpu *GBCPU) getValCartAddr(a1, a2 *byte) byte {
-	return GbMMU.Cart.MBC[binary.LittleEndian.Uint16([]byte{*a1, *a2})]
+func (gbcpu *GBCPU) getValCartAddr(a1, a2 *byte, number uint16) []byte {
+	begin := binary.LittleEndian.Uint16([]byte{*a1, *a2})
+	end := begin + (number - 1)
+	return GbMMU.Cart.MBC[begin:end]
 }
