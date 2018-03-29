4c9f3c688f8486bbab724b5cc475cfa3a5340f1c
Nicolas S. Montanaro <nsmon@rbox.co>
2018-03-29 12:35:40 -0400

    fill out CB instruction executors, add (HL) CB instructions

--- cpu/executors-cb.go
+++ cpu/executors-cb.go
@@ -1,6 +1,8 @@
 package cpu
 
 import (
+	"encoding/binary"
+
 	"../mmu"
 )
 
@@ -35,6 +37,14 @@ func (gbcpu *GBCPU) RLCr(reg *byte) {
 	gbcpu.Regs.clearHalfCarry()
 }
 
+// RLCHL -> e.g. RLC (HL)
+// Performs 8-bit rotation to the left of value at address (HL)
+// Rotated bit is copied to carry
+// Flags: Z00C
+func (gbcpu *GBCPU) RLCHL() {
+	gbcpu.RLCr(&GbMMU.Memory[binary.LittleEndian.Uint16([]byte{gbcpu.Regs.h, gbcpu.Regs.l})])
+}
+
 // RRCr -> e.g. RRC B
 // Performs 8-bit rotation to the right
 // Rotated bit is copied to carry
@@ -57,6 +67,14 @@ func (gbcpu *GBCPU) RRCr(reg *byte) {
 
 	gbcpu.Regs.clearSubtract()
 	gbcpu.Regs.clearHalfCarry()
+}
+
+// RRCHL -> e.g. RRC (HL)
+// Performs 8-bit rotation to the right of value at address (HL)
+// Rotated bit is copied to carry
+// Flags: Z00C
+func (gbcpu *GBCPU) RRCHL() {
+	gbcpu.RRCr(&GbMMU.Memory[binary.LittleEndian.Uint16([]byte{gbcpu.Regs.h, gbcpu.Regs.l})])
 }
 
 // RLr -> e.g. RL B
@@ -86,6 +104,15 @@ func (gbcpu *GBCPU) RLr(reg *byte) {
 	gbcpu.Regs.clearZero()
 }
 
+// RLHL -> e.g. RL (HL)
+// Rotates value at address (HL) left through CF
+// Store old 0th bit into carry
+// Old carry becomes new 7th bit
+// Flags: Z00C
+func (gbcpu *GBCPU) RLHL() {
+	gbcpu.RLr(&GbMMU.Memory[binary.LittleEndian.Uint16([]byte{gbcpu.Regs.h, gbcpu.Regs.l})])
+}
+
 // RRr -> e.g. RR B
 // Rotates register right through CF
 // Store old 0th bit into carry
@@ -111,6 +138,15 @@ func (gbcpu *GBCPU) RRr(reg *byte) {
 	gbcpu.Regs.clearSubtract()
 	gbcpu.Regs.clearHalfCarry()
 	gbcpu.Regs.clearZero()
+}
+
+// RRHL -> e.g. RR (HL)
+// Rotates value at address (HL) right through CF
+// Store old 0th bit into carry
+// Old carry becomes new 7th bit
+// Flags: Z00C
+func (gbcpu *GBCPU) RRHL() {
+	gbcpu.RRr(&GbMMU.Memory[binary.LittleEndian.Uint16([]byte{gbcpu.Regs.h, gbcpu.Regs.l})])
 }
 
 // SLAr -> e.g. SLA B
@@ -139,6 +175,52 @@ func (gbcpu *GBCPU) SLAr(reg *byte) {
 	}
 }
 
+// SLAHL -> e.g. SLA (HL)
+// Shift value at addr (HL) left into carry
+// Least significant bit of reg set to 0
+// Flags: Z00C
+func (gbcpu *GBCPU) SLAHL() {
+	gbcpu.SLAr(&GbMMU.Memory[binary.LittleEndian.Uint16([]byte{gbcpu.Regs.h, gbcpu.Regs.l})])
+}
+
+// SRAr -> e.g. SRA B
+// Shift reg right into carry
+// Most significant bit of reg is unaffected
+// Flags: Z000
+func (gbcpu *GBCPU) SRAr(reg *byte) {
+	*reg = (*reg >> 1) | (*reg & 0x80)
+
+	if *reg == 0 {
+		gbcpu.Regs.setZero()
+	} else {
+		gbcpu.Regs.clearZero()
+	}
+
+	gbcpu.Regs.clearCarry()
+}
+
+// SRAHL -> e.g. SRA (HL)
+// Shift value at addr (HL) right into carry
+// Most significant bit of reg is unaffected
+// Flags: Z000
+func (gbcpu *GBCPU) SRAHL() {
+	gbcpu.SRAr(&GbMMU.Memory[binary.LittleEndian.Uint16([]byte{gbcpu.Regs.h, gbcpu.Regs.l})])
+}
+
+// SWAPr -> e.g. SWAP B
+// Swap nibbles of reg
+// Flags: Z000
+func (gbcpu *GBCPU) SWAPr(reg *byte) {
+	*reg = (*reg << 4) | (*reg >> 4)
+}
+
+// SWAPHL -> e.g. SWAP (HL)
+// Swap nibbles of value at addr (HL)
+// Flags: Z000
+func (gbcpu *GBCPU) SWAPHL() {
+	gbcpu.SWAPr(&GbMMU.Memory[binary.LittleEndian.Uint16([]byte{gbcpu.Regs.h, gbcpu.Regs.l})])
+}
+
 // SRLr -> e.g. SRL B
 // Shift reg right into carry
 // Most significant bit of reg is set to 0
@@ -165,27 +247,12 @@ func (gbcpu *GBCPU) SRLr(reg *byte) {
 	}
 }
 
-// SRAr -> e.g. SRA B
-// Shift reg right into carry
-// Most significant bit of reg is unaffected
-// Flags: Z000
-func (gbcpu *GBCPU) SRAr(reg *byte) {
-	*reg = (*reg >> 1) | (*reg & 0x80)
-
-	if *reg == 0 {
-		gbcpu.Regs.setZero()
-	} else {
-		gbcpu.Regs.clearZero()
-	}
-
-	gbcpu.Regs.clearCarry()
-}
-
-// SWAPr -> e.g. SWAP B
-// Swap nibbles of reg
-// Flags: Z000
-func (gbcpu *GBCPU) SWAPr(reg *byte) {
-	*reg = (*reg << 4) | (*reg >> 4)
+// SRLHL -> e.g. SRL (HL)
+// Shift value at addr (HL) right into carry
+// Most significant bit of reg is set to 0
+// Flags: Z00C
+func (gbcpu *GBCPU) SRLHL() {
+	gbcpu.SRLr(&GbMMU.Memory[binary.LittleEndian.Uint16([]byte{gbcpu.Regs.h, gbcpu.Regs.l})])
 }
 
 // BITnr -> e.g. BIT 0,B
@@ -204,6 +271,13 @@ func (gbcpu *GBCPU) BITnr(pos uint8, reg *byte) {
 	gbcpu.Regs.setHalfCarry()
 }
 
+// BITHL -> e.g. BIT 0,(HL)
+// Test bit at position in value at addr (HL)
+// Flags: Z01-
+func (gbcpu *GBCPU) BITHL(pos uint8) {
+	gbcpu.BITnr(pos, &GbMMU.Memory[binary.LittleEndian.Uint16([]byte{gbcpu.Regs.h, gbcpu.Regs.l})])
+}
+
 // RESnr -> e.g. RES 0,B
 // Reset bit in register
 // Flags: ----
@@ -211,9 +285,23 @@ func (gbcpu *GBCPU) RESnr(pos uint8, reg *byte) {
 	*reg &^= (1 << pos)
 }
 
+// RESHL -> e.g. RES 0,(HL)
+// Reset bit in value at addr (HL)
+// Flags: ----
+func (gbcpu *GBCPU) RESHL(pos uint8) {
+	gbcpu.RESnr(pos, &GbMMU.Memory[binary.LittleEndian.Uint16([]byte{gbcpu.Regs.h, gbcpu.Regs.l})])
+}
+
 // SETnr -> e.g. SET 0,B
 // Set bit in register
 // Flags: ----
 func (gbcpu *GBCPU) SETnr(pos uint8, reg *byte) {
 	*reg |= (1 << pos)
+}
+
+// SETHL -> e.g. SET 0,(HL)
+// Set bit in value at addr (HL)
+// Flags: ----
+func (gbcpu *GBCPU) SETHL(pos uint8) {
+	gbcpu.SETnr(pos, &GbMMU.Memory[binary.LittleEndian.Uint16([]byte{gbcpu.Regs.h, gbcpu.Regs.l})])
 }
